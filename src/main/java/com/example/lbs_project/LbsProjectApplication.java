48cbfbac520123c7725806af7ea7df7d222651a4
Sefahmet <sefaaltundal@gmail.com>
2023-06-22 12:35:34 +0200

    Initial commit

---
+++ src/main/java/com/example/lbs_project/LbsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.lbs_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lbs_project/LbsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lbs_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
