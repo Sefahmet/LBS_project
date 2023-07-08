4ead56317283c9174f4767fa67bbab1569535d3f
Sefahmet <sefaaltundal@gmail.com>
2023-07-08 20:11:21 +0200

    benwa update

--- src/main/java/com/example/lbs_project/Controller/ShortestPathController.java
+++ src/main/java/com/example/lbs_project/Controller/ShortestPathController.java
@@ -74,4 +74,45 @@ public class ShortestPathController {
             return new ResponseEntity(directoryLocation + e, HttpStatus.BAD_REQUEST);
         }
     }
+    @GetMapping(value = "/forbenwa")
+    public ResponseEntity<Path> getShortestPathWLatLontest(@Valid @RequestParam double lat1,
+                                                       @Valid @RequestParam double lon1,
+                                                       @Valid @RequestParam double lat2,
+                                                       @Valid @RequestParam double lon2,
+                                                       @Valid @RequestParam double w1,
+                                                       @Valid @RequestParam double w2,
+                                                       @Valid @RequestParam double w3,
+                                                       @Valid @RequestParam double w4) throws IOException {
+
+        String directoryLocation = "";
+        try {
+            String resourcePath = "classpath:Data";
+            Resource resource = resourceLoader.getResource(resourcePath);
+            File dataFile = resource.getFile();
+            java.nio.file.Path directoryPath = Paths.get(dataFile.getParent());
+            directoryLocation = directoryPath.toAbsolutePath().toString();
+
+            Coordinate p1 = shortestPathService.LatLon2EN(lat1, lon1);
+            Coordinate p2 = shortestPathService.LatLon2EN(lat2, lon2);
+
+            double x1 = p1.getX();
+            double y1 = p1.getY();
+            double x2 = p2.getX();
+            double y2 = p2.getY();
+            MyDataSingleton data = shortestPathService.getShortestPathCoordinates(x1, y1, x2, y2, directoryLocation);
+
+            List<Coordinate> coords = ShortestPathService.shortestPath2VisualCoords(data, x1, y1, x2, y2);
+            List<Coordinate> rescoords = new ArrayList<>();
+            for (Coordinate coord : coords) {
+                rescoords.add(shortestPathService.EN2LatLon(coord.getX(), coord.getY()));
+            }
+            UUID pathId = UUID.randomUUID();
+            RouteDataStorage.getInstance().put(pathId, coords);
+            Path responsePath = new Path(pathId, rescoords);
+
+            return new ResponseEntity<>(responsePath, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(directoryLocation + e, HttpStatus.BAD_REQUEST);
+        }
+    }
 }
